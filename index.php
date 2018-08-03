8ffb1e101ee1c2e59503887f373254f01aeaf192
Natália Marise da Silva <natthy.marise@gmail.com>
2018-08-02 21:58:10 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 
 require_once("config.php");
-$sql = new Sql();
-$usuarios = $sql -> select("SELECT *FROM tb_usuarios");
-echo json_encode($usuarios);
+
+//Carrega o usuario
+//$root = new Usuario();
+//$root -> loadbyId(3);
+//echo $root;
+
+//Carrega lista de usuarios
+//$lista = Usuario::getList();
+//echo json_decode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha
+
+$usuario = new Usuario();
+$usuario -> login("root", "!@#$");
+
+echo $usuario;
+
+
 ?>
--- sql.php
+++ sql.php
@@ -14,11 +14,11 @@ class Sql extends PDO
 
 	private function setParams($statment, $parameters = array()){
 		foreach($parameters as $key => $value){
-           $this -> setParam($key, $value);
+           $this -> setParam($statement, $key, $value);
 		}
 	}
-	private function setParam($statment, $key, $value){
-		$statment-> bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement-> bindParam($key, $value);
 	}
 
 	public function query($rawQuery, $params = array()){
